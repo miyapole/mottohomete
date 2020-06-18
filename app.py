4d151ae0e2b3264e718526bfb0e1ba6deead02e8
miyapole <asamiyyy@gmail.com>
2020-06-18 20:23:32 +0900

    ビールうまい

--- app.py
+++ app.py
@@ -13,3 +13,15 @@ app.secret_key = "sunabaco"
 @app.route("/")
 def helloworld():
     return "Hello World."
+
+
+@app.errorhandler(404)
+def notfound(code):
+    return "404です。月に代わってお仕置きよ！！(CV:三石琴乃)"
+
+
+
+
+if __name__ == "__main__":
+    #Flaskが持っている共用サーバを実行します
+    app.run(debug=True)
